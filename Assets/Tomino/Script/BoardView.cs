aeb7f28c6f29074d1ec6eb3b3509ca68dc6efb41
JOHNNY LIN <linjqwer@gmail.com>
2022-07-16 22:31:13 -0400

    change sprites

--- Assets/Tomino/Script/BoardView.cs
+++ Assets/Tomino/Script/BoardView.cs
@@ -6,7 +6,7 @@ public class BoardView : MonoBehaviour
 {
     enum Layer
     {
-        Blocks, TargetOutline, PieceShadow
+        Blocks, PieceShadow, TargetOutline
     }
 
     public GameObject blockPrefab;
